ccac0da2df38076be09bccba7b48abf55a3bf5ca
SUsmna <159513831+SUsmna@users.noreply.github.com>
2024-05-28 10:58:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-function makeid(l) {
+function makeid(length) {
   // write your code here
 	const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 	let result = ' ';
     const charactersLength = characters.length;
-    for ( let i = 0; i < l; i++ ) {
+    for ( let i = 0; i < length; i++ ) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
-	return result
+	return result;
 }
 
 // Do not change the code below.
